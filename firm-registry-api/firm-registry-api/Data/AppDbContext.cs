089060ba4f36a8bed2c3c924fa6c519038095254
Sonja <Sonja@DESKTOP-NNFELFS>
2025-09-17 16:35:27 +0200

    Database connection set.

---
+++ firm-registry-api/firm-registry-api/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace firm_registry_api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<WeatherForecast> WeatherForecasts { get; set; }
+        
+    
+    }
+}
